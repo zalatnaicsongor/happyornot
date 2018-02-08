4669f74d2a8602ee00a650039e9344c2e9271865
Csongor Zalatnai <csongor@zalatnai.hu>
2018-02-08 21:50:13 +0000

    renamed app

--- src/main/kotlin/hu/zalatnai/happyornot/HappyornotApplication.kt
+++ src/main/kotlin/hu/zalatnai/happyornot/HappyornotApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HappyornotProducerApplication
+class HappyornotApplication
 
 fun main(args: Array<String>) {
-    runApplication<HappyornotProducerApplication>(*args)
+    runApplication<HappyornotApplication>(*args)
 }
